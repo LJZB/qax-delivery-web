1acfe471371f674713a5e8e263a6e6f10aa09b67
Luis Zuluaga <luiszuluaga90@gmail.com>
2026-07-06 22:41:04 -0500

    Pausas agregadas

--- Stage_2/Challenge/Ch1/playwright.config.ts
+++ Stage_2/Challenge/Ch1/playwright.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   timeout: 30000,
   retries: 1,
-  reporter: 'html',
+  reporter: "html",
   use: {
     headless: true,
-    screenshot: 'only-on-failure',
-    trace: 'on-first-retry',
+    screenshot: "only-on-failure",
+    trace: "on-first-retry",
   },
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
   ],
 });
